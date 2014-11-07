b40bfbdc5d6691da3729bf3d3878685978cf021c
Tomas Hertus <kittrcz@gmail.com>
2014-11-07 13:32:36 -0800

    Update stocks.rb

--- stocks.rb
+++ stocks.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
 require 'yahoo_finance'
-stocks = ['AMZN','TWTR','MSFT','BABA','WMT','DIS','FB','TSLA']
+
+STOCK_FILE = '.stocks'
+
+stocks = []
+IO.readlines(STOCK_FILE).each do |line|
+  stocks << line.delete("\n")
+end
+
 YahooFinance.quotes(stocks, [:last_trade_price]).each{|x| puts x.symbol + ': ' + x.last_trade_price}
